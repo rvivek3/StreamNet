7c15a8a9c4e13cc41a9f420b4c5f844588c14557
Rajan Vivek <rajanvivek717@gmail.com>
2021-11-01 11:46:32 -0400

    getting file structure and relevant code

---
+++ utils/utils.py
@@ -0,0 +1,64 @@
+import numpy as np
+import torch
+
+
+class MergeLayer(torch.nn.Module):
+  def __init__(self, dim1, dim2, dim3, dim4):
+    super().__init__()
+    self.fc1 = torch.nn.Linear(dim1 + dim2, dim3)
+    self.fc2 = torch.nn.Linear(dim3, dim4)
+    self.act = torch.nn.ReLU()
+
+    torch.nn.init.xavier_normal_(self.fc1.weight)
+    torch.nn.init.xavier_normal_(self.fc2.weight)
+
+  def forward(self, x1, x2):
+    x = torch.cat([x1, x2], dim=1)
+    h = self.act(self.fc1(x))
+    return self.fc2(h)
+
+
+class MLP(torch.nn.Module):
+  def __init__(self, dim, drop=0.3):
+    super().__init__()
+    self.fc_1 = torch.nn.Linear(dim, 80)
+    self.fc_2 = torch.nn.Linear(80, 10)
+    self.fc_3 = torch.nn.Linear(10, 1)
+    self.act = torch.nn.ReLU()
+    self.dropout = torch.nn.Dropout(p=drop, inplace=False)
+
+  def forward(self, x):
+    x = self.act(self.fc_1(x))
+    x = self.dropout(x)
+    x = self.act(self.fc_2(x))
+    x = self.dropout(x)
+    return self.fc_3(x).squeeze(dim=1)
+
+
+class EarlyStopMonitor(object):
+  def __init__(self, max_round=3, higher_better=True, tolerance=1e-10):
+    self.max_round = max_round
+    self.num_round = 0
+
+    self.epoch_count = 0
+    self.best_epoch = 0
+
+    self.last_best = None
+    self.higher_better = higher_better
+    self.tolerance = tolerance
+
+  def early_stop_check(self, curr_val):
+    if not self.higher_better:
+      curr_val *= -1
+    if self.last_best is None:
+      self.last_best = curr_val
+    elif (curr_val - self.last_best) / np.abs(self.last_best) > self.tolerance:
+      self.last_best = curr_val
+      self.num_round = 0
+      self.best_epoch = self.epoch_count
+    else:
+      self.num_round += 1
+
+    self.epoch_count += 1
+
+    return self.num_round >= self.max_round
